ac168cd4e491ac3b32ee27c2a05d6c4315ff3f17
vyshnaviellendula <vysh785@gmail.com>
2025-07-13 16:16:51 +0530

    Add Week4_Spring_REST using Spring Boot folder to learning-program-solutions

---
+++ learning-program-solutions/Week4_Spring_REST using Spring Boot/spring-rest-handson/04_REST - Country Web Service/code/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,30 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.debug("START main()");
+		SpringApplication.run(SpringLearnApplication.class, args);
+
+		displayCountry();
+
+		LOGGER.debug("END main()");
+	}
+
+	public static void displayCountry() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = (Country) context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+	}
+}
+
